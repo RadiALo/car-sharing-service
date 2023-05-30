025a8b06bb8f98334f4fee128900ab23af5d07f4
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 15:09:34 +0300

    Implemented service and repository for Rental.

---
+++ src/main/java/com/carsharing/service/RentalService.java
@@ -0,0 +1,12 @@
+package com.carsharing.service;
+
+import com.carsharing.model.Rental;
+import java.util.List;
+
+public interface RentalService {
+    Rental save(Rental rental);
+
+    Rental get(Long id);
+
+    List<Rental> getByUserId(Long id);
+}

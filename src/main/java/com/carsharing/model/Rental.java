46408006edfda216b0dce84128c8a8ada298be5c
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 11:39:04 +0300

    Revert "Refactored entity"
    This reverts commit a5e6d071fef7a29c9f820f29d03333bc630b3987.

--- src/main/java/com/carsharing/model/Rental.java
+++ src/main/java/com/carsharing/model/Rental.java
@@ -2,21 +2,16 @@ package com.carsharing.model;
 
 import java.util.Date;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
-@EqualsAndHashCode
+@Data
 @Table(name = "rentals")
 public class Rental {
     @Id
@@ -25,19 +20,10 @@ public class Rental {
     private Date rentalDate;
     private Date returnDate;
     private Date actualReturnDate;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "car_id")
     private Car car;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
-
-    @Override
-    public String toString() {
-        return "Rental{"
-                + "id=" + id
-                + ", rentalDate=" + rentalDate
-                + ", returnDate=" + returnDate
-                + ", actualReturnDate=" + returnDate + '}';
-    }
 }

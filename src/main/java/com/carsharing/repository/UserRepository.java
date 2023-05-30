baec5ee6a2bd45d41536a09568f5c56ea420a239
Danilo Kozakov <78644882+RadiALo@users.noreply.github.com>
2023-05-30 09:49:09 +0300

    User entity with repo, service and mapper

---
+++ src/main/java/com/carsharing/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.carsharing.repository;
+
+import com.carsharing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}

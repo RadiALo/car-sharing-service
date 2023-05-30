91c14449ee42b88aa67fc2be4feb0db720ed447e
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 18:51:54 +0300

    Added RentalController, properties to correct time format input/output, added util method for correct data display in setActualReturnDate method response.

---
+++ src/main/java/com/carsharing/controller/RentalController.java
@@ -0,0 +1,52 @@
+package com.carsharing.controller;
+
+import com.carsharing.dto.request.RentalRequestDto;
+import com.carsharing.dto.response.RentalResponseDto;
+import com.carsharing.model.Rental;
+import com.carsharing.service.RentalService;
+import com.carsharing.service.mapper.RequestMapper;
+import com.carsharing.service.mapper.ResponseMapper;
+import com.carsharing.util.DataUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rentals")
+public class RentalController {
+    private final RentalService rentalService;
+    private final RequestMapper<RentalRequestDto, Rental> requestMapper;
+    private final ResponseMapper<RentalResponseDto, Rental> responseMapper;
+    private final DataUtil dataUtil;
+
+    @PostMapping
+    public RentalResponseDto add(@RequestBody RentalRequestDto requestDto) {
+        return responseMapper
+                .fromModel(rentalService.save(requestMapper.toModel(requestDto)));
+    }
+
+    @GetMapping("/{id}")
+    public RentalResponseDto findById(@PathVariable Long id) {
+        return responseMapper.fromModel(rentalService.get(id));
+    }
+
+    @GetMapping("/user_id={userId}")
+    public List<RentalResponseDto> findByUserId(@PathVariable Long userId) {
+        return rentalService.getByUserId(userId)
+                .stream()
+                .map(responseMapper::fromModel)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/{id}/return/")
+    public RentalResponseDto setActualReturnDate(@PathVariable Long id,
+                                                 @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date actualTime) {
+        Rental rental = rentalService.get(id);
+        rental.setActualReturnDate(dataUtil.currentData(actualTime));
+        return responseMapper.fromModel(rental);
+    }
+}

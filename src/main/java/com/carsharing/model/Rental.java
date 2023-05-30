a5e6d071fef7a29c9f820f29d03333bc630b3987
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 11:35:48 +0300

    Refactored entity

--- src/main/java/com/carsharing/model/Rental.java
+++ src/main/java/com/carsharing/model/Rental.java
@@ -2,16 +2,21 @@ package com.carsharing.model;
 
 import java.util.Date;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @Table(name = "rentals")
 public class Rental {
     @Id
@@ -20,10 +25,19 @@ public class Rental {
     private Date rentalDate;
     private Date returnDate;
     private Date actualReturnDate;
-    @ManyToOne
-    @JoinColumn(name = "car_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
     private Car car;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
     private User user;
+
+    @Override
+    public String toString() {
+        return "Rental{"
+                + "id=" + id
+                + ", rentalDate=" + rentalDate
+                + ", returnDate=" + returnDate
+                + ", actualReturnDate=" + returnDate + '}';
+    }
 }

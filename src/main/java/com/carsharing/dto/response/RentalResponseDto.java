5d119664b45af387ee1e18df71f78803335fef29
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 16:12:02 +0300

    Implemented mapper, response and request dto for Rental.

--- src/main/java/com/carsharing/dto/response/RentalResponseDto.java
+++ src/main/java/com/carsharing/dto/response/RentalResponseDto.java
@@ -1,13 +1,15 @@
-package com.carsharing.dto.request;
+package com.carsharing.dto.response;
 
 import lombok.Data;
 import java.util.Date;
 
 @Data
-public class RentalRequestDto {
+public class RentalResponseDto {
+    private Long id;
     private Date rentalDate;
     private Date returnDate;
     private Date actualReturnDate;
     private Long carId;
     private Long userId;
 }
+

54fa2452fcc70880c4436a5ec9f69fd2dcccc5f3
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-05-30 13:09:04 +0300

    Revert "Implemented service and repository for Rental"
    This reverts commit 2b73218156ab6fce335de1d55bf561c3326abae9.

--- src/main/java/com/carsharing/repository/RentalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carsharing.repository;
-
-import com.carsharing.model.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-    List<Rental> getRentalsByUserId(Long id);
-}

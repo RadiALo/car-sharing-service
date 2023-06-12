3107eedb32cdb22f327c91002a20d113345fe01d
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-06-12 12:09:07 +0300

    Implemented tests for Payment strategy fine.

---
+++ src/test/java/com/example/carsharing/service/PaymentStrategyFineTest.java
@@ -0,0 +1,78 @@
+package com.example.carsharing.service;
+
+import com.carsharing.model.Car;
+import com.carsharing.model.Rental;
+import com.carsharing.model.User;
+import com.carsharing.service.RentalService;
+import com.carsharing.service.impl.PaymentStrategyFine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class PaymentStrategyFineTest {
+    private Rental testRental;
+    private User testUser;
+    private Car testCar;
+    private BigDecimal dailyFee;
+
+    @InjectMocks
+    private PaymentStrategyFine payment;
+
+    @Mock
+    private RentalService rentalService;
+
+    @BeforeEach
+    void init() {
+        testUser = new User();
+        testUser.setId(1L);
+        testUser.setFirstName("testFirstName");
+        testUser.setSecondName("testSecondName");
+        testUser.setPassword("testPassword");
+        testUser.setEmail("testEmail");
+        testUser.setRole(User.Role.CUSTOMER);
+
+        testCar = new Car();
+        testCar.setId(1L);
+        testCar.setType(Car.Type.SEDAN);
+        testCar.setModel("model");
+        testCar.setBrand("brand");
+        testCar.setInventory(5);
+        testCar.setDailyFee(BigDecimal.valueOf(5.0));
+
+        testRental = new Rental();
+        testRental.setId(1L);
+        testRental.setUser(testUser);
+        testRental.setCar(testCar);
+        testRental.setRentalDate(LocalDate.now());
+        testRental.setActive(true);
+        testRental.setReturnDate(LocalDate.of(2023, 06, 13));
+
+        dailyFee = testRental.getCar().getDailyFee();
+    }
+
+    @Test
+    void carNotReturned_notOk() {
+        Mockito.when(rentalService.get(any(Long.class))).thenReturn(testRental);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> payment.calculateFineByType(testRental.getId()));
+    }
+
+    @Test
+    void carReturned_ok() {
+        testRental.setActualReturnDate(LocalDate.of(2023, 06, 13));
+        Mockito.when(rentalService.get(any(Long.class))).thenReturn(testRental);
+        Assertions.assertEquals(BigDecimal.valueOf(-500),
+                payment.calculateFineByType(testRental.getId()));
+    }
+}
